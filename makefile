3829b6103db57e04383b49b3ba49dd03f7c0005d
Saksham Gupta <sakshamgupta05@gmail.com>
2019-04-15 20:46:53 +0530

    added timeouts

--- makefile
+++ makefile
@@ -4,4 +4,4 @@ dir:
 	mkdir bin
 
 bin/udp_select: src/udp_select.c
-	gcc -o bin/udp_select src/udp_select.c
+	gcc -o bin/udp_select -lrt src/udp_select.c
--- src/udp_select.c
+++ src/udp_select.c
@@ -1,3 +1,6 @@
+#define _POSIX_C_SOURCE 199309
+#include <signal.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,17 +12,128 @@
 
 #define BUF_SIZE 516
 #define MODE_MAX 9
+#define MAX_TIMEOUT 3
 
 struct req {
   int sock_fd;
+  struct sockaddr_in claddr;
   FILE *file_fs;
   int block_num;
+  int n_read;
   int last;
+  int n_timeout;
+  timer_t timer_id;
 };
 
 struct req *requests[65536];
 
 char buf[BUF_SIZE];
+fd_set readfds;
+
+void deleteTimer(struct req *r) {
+  timer_delete(r -> timer_id);
+}
+
+void free_r(struct req *r) {
+  deleteTimer(r);
+  FD_CLR(r -> sock_fd, &readfds);
+  close(r -> sock_fd);
+  fclose(r -> file_fs);
+  free(r);
+}
+
+void sendErr(int fd, struct sockaddr_in *claddr, char *errStr, int ec) {
+  printf("%s\n", errStr);
+  int op = htons(5);
+  memcpy(buf, &op, 2);
+  int ec_n = htons(ec);
+  memcpy(buf + 2, &ec_n, 2);
+  int numBytes = strlen(errStr) + 1;
+  sprintf(buf + 4, "%s", errStr);
+  socklen_t len = sizeof(struct sockaddr_in);
+  if (sendto(fd, buf, 4 + numBytes, 0, (struct sockaddr *) claddr, len) != numBytes + 4) {
+    perror("sendto");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void logRecv(struct sockaddr_in *claddr, int numBytes, int opcode) {
+  char claddrStr[INET_ADDRSTRLEN];
+  if (inet_ntop(AF_INET, &(claddr -> sin_addr), claddrStr, INET_ADDRSTRLEN) == NULL) {
+    printf("Couldn't convert client address to string\n");
+  } else {
+    printf("Server received opcode: %d, numBytes: %ld from (%s, %u)\n",
+        opcode, (long) numBytes, claddrStr, ntohs(claddr -> sin_port));
+  }
+}
+
+void logSend(struct req *r) {
+  char claddrStr[INET_ADDRSTRLEN];
+  if (inet_ntop(AF_INET, &(r -> claddr.sin_addr), claddrStr, INET_ADDRSTRLEN) == NULL) {
+    printf("Couldn't convert client address to string\n");
+  } else {
+    printf("Server sent block: %d, numBytes: %ld to (%s, %u)\n",
+        r -> block_num, (long) r -> n_read, claddrStr, ntohs(r -> claddr.sin_port));
+  }
+}
+
+void resetTimer(struct req *r) {
+  struct timespec interval;
+  interval.tv_sec = 0;
+  interval.tv_nsec = 0;
+  struct timespec value;
+  value.tv_sec = 5;
+  value.tv_nsec = 0;
+  struct itimerspec its;
+  its.it_interval = value;
+  its.it_value = value;
+  timer_settime(r -> timer_id, 0, &its , NULL);
+}
+
+void sendBlock(struct req *r) {
+  int op = htons(3);
+  memcpy(buf, &op, 2);
+  int blockNum_n = htons(r -> block_num);
+  memcpy(buf + 2, &blockNum_n, 2);
+  size_t numBytes = fread(buf + 4, 1, 512, r -> file_fs);
+
+  socklen_t len = sizeof(struct sockaddr_in);
+  if (sendto(r -> sock_fd, buf, 4 + numBytes, 0, (struct sockaddr *) &(r ->claddr), len) != numBytes + 4) {
+    perror("sendto");
+    exit(EXIT_FAILURE);
+  }
+
+  r -> n_read = numBytes;
+  logSend(r);
+  if (numBytes < 512) {
+    r -> last = 1;
+  }
+}
+
+void timeout(union sigval sv) {
+  struct req *r = requests[sv.sival_int];
+  if (r -> n_timeout >= MAX_TIMEOUT) {
+    sendErr(r -> sock_fd, &(r -> claddr), "max retries exceeded", 4);
+    free_r(r);
+    return;
+  }
+
+  printf("retry: %d, sfd: %d\n", r -> n_timeout + 1, r -> sock_fd);
+  fseek(r -> file_fs, -1 * r -> n_read, SEEK_CUR);
+  sendBlock(r);
+  r -> n_timeout++;
+}
+
+void createTimer(struct req *r, int arg) {
+  union sigval sigval;
+  sigval.sival_int = arg;
+  struct sigevent sev;
+  sev.sigev_notify = SIGEV_THREAD;
+  sev.sigev_value = sigval;
+  sev.sigev_notify_function = timeout;
+  sev.sigev_notify_attributes = NULL;
+  timer_create(CLOCK_REALTIME, &sev, &(r -> timer_id));
+}
 
 char* getString(char *dst, char *src) {
   while (*src != '\0') {
@@ -47,18 +161,16 @@ void rrq(char *filename, char *mode, struct sockaddr_in claddr) {
 
 int main(int argc, char* argv[]) {
   struct sockaddr_in svaddr, claddr;
-  char claddrStr[INET_ADDRSTRLEN];
   char svaddrStr[INET_ADDRSTRLEN];
   int sfd;
   int nfds;
-  fd_set readfds;
   FD_ZERO(&readfds);
 
   if (argc != 2) {
     printf("provide a port number\n");
     exit(EXIT_FAILURE);
   }
-  int port = atoi(argv[1]);
+  int port_sv = atoi(argv[1]);
 
   sfd = socket(AF_INET, SOCK_DGRAM, 0);
   if (sfd == -1) {
@@ -72,7 +184,7 @@ int main(int argc, char* argv[]) {
   memset(&svaddr, 0, sizeof(struct sockaddr_in));
   svaddr.sin_family = AF_INET;
   svaddr.sin_addr = inaddr_any;
-  svaddr.sin_port = htons(port);
+  svaddr.sin_port = htons(port_sv);
 
   if (bind(sfd, (struct sockaddr *) &svaddr, sizeof(struct sockaddr_in)) == -1) {
     perror("bind");
@@ -88,12 +200,11 @@ int main(int argc, char* argv[]) {
 
   for (;;) {
     select(nfds, &readfds, NULL, NULL, 0);
-    fflush(stdout);
     for (int fd = 0; fd < nfds; fd++) {
-      printf("--->%d\n", fd);
       if (FD_ISSET(fd, &readfds)) {
-        printf("===>%d\n", fd);
         if (fd == sfd) {
+          // new request
+
           socklen_t len = sizeof(struct sockaddr_in);
           int numBytes = recvfrom(sfd, buf, BUF_SIZE, 0, (struct sockaddr *) &claddr, &len);
           if (numBytes == -1) {
@@ -101,19 +212,12 @@ int main(int argc, char* argv[]) {
             exit(EXIT_FAILURE);
           }
 
-          // LOG: new connection
-          if (inet_ntop(AF_INET, &claddr.sin_addr, claddrStr, INET_ADDRSTRLEN) == NULL) {
-            printf("Couldn't convert client address to string\n");
-          } else {
-            printf("Server received %ld bytes from (%s, %u)\n",
-                (long) numBytes, claddrStr, ntohs(claddr.sin_port));
-          }
-
           int opcode_n = 0;
           int opcode = 0;
           memcpy(&opcode_n, buf, 2);
           opcode = ntohs(opcode_n);
-          printf("opcode: %d\n", opcode);
+
+          logRecv(&claddr, numBytes, opcode);
           if (opcode == 4){
             // ack
             int bn_n = 0;
@@ -133,77 +237,48 @@ int main(int argc, char* argv[]) {
           char filename[PATH_MAX];
           char mode[MODE_MAX];
           parseReq(buf, filename, mode);
-          printf("opcode: %d\n", opcode);
           printf("filename: %s\n", filename);
           printf("mode: %s\n", mode);
           int modeLen = strlen(mode);
           for (int i = 0; i < modeLen; i++) mode[i] = tolower(mode[i]);
           if (strncmp(mode, "octet", 5) != 0) {
-            char *errStr = "only octet mode supported";
-            printf("%s\n", errStr);
-            int op = htons(5);
-            memcpy(buf, &op, 2);
-            int ec = htons(4);
-            memcpy(buf + 2, &ec, 2);
-            int numBytes = strlen(errStr) + 1;
-            sprintf(buf + 4, "%s", errStr);
-            if (sendto(sfd, buf, 4 + numBytes, 0, (struct sockaddr *) &claddr, len) != numBytes + 4) {
-              perror("sendto");
-              exit(EXIT_FAILURE);
-            }
+            sendErr(sfd, &claddr, "only octet mode supported", 4);
             continue;
           }
           if (opcode == 1) {
             // rrq
+            FILE *f = fopen(filename, "r");
+            if (f == NULL) {
+              sendErr(sfd, &claddr, "file not found", 1);
+              continue;
+            }
+
             int sfd1 = socket(AF_INET, SOCK_DGRAM, 0);
             if (sfd1 == -1) {
               perror("socket");
               exit(EXIT_FAILURE);
             }
-
-            FILE *f = fopen(filename, "r");
-            if (f == NULL) {
-              char *errStr = "file not found";
-              printf("%s\n", errStr);
-              int op = htons(5);
-              memcpy(buf, &op, 2);
-              int ec = htons(1);
-              memcpy(buf + 2, &ec, 2);
-              int numBytes = strlen(errStr) + 1;
-              sprintf(buf + 4, "%s", errStr);
-              if (sendto(sfd, buf, 4 + numBytes, 0, (struct sockaddr *) &claddr, len) != numBytes + 4) {
-                perror("sendto");
-                exit(EXIT_FAILURE);
-              }
-              close(sfd1);
-              continue;
-            }
-            int op = htons(3);
-            memcpy(buf, &op, 2);
-            int blockNum = htons(1);
-            memcpy(buf + 2, &blockNum, 2);
-            size_t numBytes = fread(buf + 4, 1, 512, f);
-            printf("block: %d, numBytes: %d\n", blockNum, (int) numBytes);
-
-            socklen_t len = sizeof(struct sockaddr_in);
-            if (sendto(sfd1, buf, 4 + numBytes, 0, (struct sockaddr *) &claddr, len) != numBytes + 4) {
-              perror("sendto");
-              exit(EXIT_FAILURE);
-            }
-            getsockname(sfd1, (struct sockaddr *) &svaddr, &len);
-            int port = ntohs(svaddr.sin_port);
-            printf("port: %d\n", port);
-            struct req *r = malloc(sizeof(struct req));
-            r -> sock_fd = sfd1;
-            r -> file_fs = f;
-            r -> block_num = 1;
-            if (numBytes < 512) r -> last = 1;
-            requests[port] = r;
-
             nfds = sfd1 + 1;
             FD_SET(sfd1, &readfds);
+
+            struct req *r = malloc(sizeof(struct req));
+            r -> sock_fd = sfd1;
+            r -> claddr = claddr;
+            r -> file_fs = f;
+            r -> block_num = 1;
+            r -> n_timeout = 0;
+
+            sendBlock(r);
+
+            getsockname(sfd1, (struct sockaddr *) &svaddr, &len);
+            int port = ntohs(svaddr.sin_port);
+            requests[port] = r;
+
+            createTimer(r, port);
+            resetTimer(r);
           }
         } else {
+          // ack (existing connection)
           socklen_t len = sizeof(struct sockaddr_in);
           int numBytes = recvfrom(fd, buf, BUF_SIZE, 0, (struct sockaddr *) &claddr, &len);
           if (numBytes == -1) {
@@ -211,24 +286,16 @@ int main(int argc, char* argv[]) {
             exit(EXIT_FAILURE);
           }
 
-          // LOG:
-          if (inet_ntop(AF_INET, &claddr.sin_addr, claddrStr, INET_ADDRSTRLEN) == NULL) {
-            printf("Couldn't convert client address to string\n");
-          } else {
-            printf("Server received %ld bytes from (%s, %u)\n",
-                (long) numBytes, claddrStr, ntohs(claddr.sin_port));
-          }
-
           getsockname(fd, (struct sockaddr *) &svaddr, &len);
           int port = ntohs(svaddr.sin_port);
-
           struct req *r = requests[port];
 
           int op_n = 0;
           int op = 0;
           memcpy(&op_n, buf, 2);
           op = ntohs(op_n);
-          /* printf("opcode: %d\n", op); */
+
+          logRecv(&claddr, numBytes, op);
           if (op == 4) {
             // ack
             int blockNum_n = 0;
@@ -239,31 +306,13 @@ int main(int argc, char* argv[]) {
             if (blockNum == r -> block_num) {
               if (r -> last) {
                 printf("complete, %d blocks transferred\n", blockNum);
-                FD_CLR(r -> sock_fd, &readfds);
-                close(r -> sock_fd);
-                fclose(r -> file_fs);
-                free(r);
-
+                free_r(r);
               } else {
-                blockNum++;
-                int op = htons(3);
-                memcpy(buf, &op, 2);
-                blockNum_n = htons(blockNum);
-                memcpy(buf + 2, &blockNum_n, 2);
-                size_t numBytes = fread(buf + 4, 1, 512, r -> file_fs);
-                /* printf("block: %d, numBytes: %d\n", blockNum, (int) numBytes); */
-
-                socklen_t len = sizeof(struct sockaddr_in);
-                if (sendto(r -> sock_fd, buf, 4 + numBytes, 0, (struct sockaddr *) &claddr, len) != numBytes + 4) {
-                  perror("sendto");
-                  exit(EXIT_FAILURE);
-                }
-                r -> block_num = blockNum;
-                if (numBytes < 512) {
-                  r -> last = 1;
-                }
+                r -> block_num++;
+                r -> n_timeout = 0;
+                sendBlock(r);
+                resetTimer(r);
               }
-
             } else {
               printf("incorrect block num, rec: %d, req: %d\n", blockNum, r -> block_num);
             }
@@ -274,12 +323,8 @@ int main(int argc, char* argv[]) {
             memcpy(&ec_n, buf + 2, 2);
             int ec = ntohs(ec_n);
             printf("error %d: %s\n", ec, buf + 4);
-            FD_CLR(r -> sock_fd, &readfds);
-            close(r -> sock_fd);
-            fclose(r -> file_fs);
-            free(r);
+            free_r(r);
           }
-
         }
       }
     }
